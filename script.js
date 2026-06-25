bcd7651e4466eea19e7a6a47f67151e3c48eade1
Kashish Nishad <nishadkashish75@gmail.com>
2026-06-25 09:30:57 +0530

    piano using html and js

---
+++ script.js
@@ -0,0 +1,150 @@
+document.addEventListener("keydown", (e) => {
+    console.log(e.key);
+
+    let btn = e.key;
+
+    if (btn === 'a') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A0.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === 'b') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A1.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === 'c') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A2.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === 'd') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A3.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === 'e') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A5.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === 'f') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A6.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === 'g') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A7.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === 'h') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A8.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === '1') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A9.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === '2') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A10.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === '3') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A11.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === '4') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A12.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+    if (btn === '5') {
+        let a = document.getElementById(e.key);
+        const A = new Audio("A13.mp3");
+        A.play()
+            .then(() => {
+                console.log("Audio played successfully");
+            })
+            .catch((error) => {
+                console.log("Error playing audio");
+            });
+    }
+
+});
